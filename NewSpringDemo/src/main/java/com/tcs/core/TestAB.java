06288ea976508faa62c820a3a18f91afa5cd01ba
Coderfaal <fnargund4@gmail.com>
2025-11-24 11:18:48 +0000

    Bean Creation from POJO and rearraging the path

--- NewSpringDemo/src/main/java/com/tcs/core/TestAB.java
+++ NewSpringDemo/src/main/java/com/tcs/core/TestAB.java
@@ -1,6 +1,7 @@
 package com.tcs.core;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class TestAB {
 
@@ -8,8 +9,9 @@ public class TestAB {
 //        B b= new B();
 //        System.out.println(b.useMethod());
         //bootstrap IOC Container
-       ClassPathXmlApplicationContext context=
-               new ClassPathXmlApplicationContext("beans.xml");
+       //ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
+        BeanFactory context = new FileSystemXmlApplicationContext("beans.xml");
+
        if (context.containsBean("a"))
        {
            A a=(A) context.getBean("a");
